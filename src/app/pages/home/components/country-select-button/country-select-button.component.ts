4c3efec5b634830dde4b28761b1da9d956257289
Fatih Sinan Yaman <fatihsinanyaman@gmail.com>
2020-04-26 15:58:10 +0300

    country select fix

--- src/app/pages/home/components/country-select-button/country-select-button.component.ts
+++ src/app/pages/home/components/country-select-button/country-select-button.component.ts
@@ -8,8 +8,30 @@ import { Country } from "src/app/interfaces";
 })
 export class CountrySelectButtonComponent implements OnInit {
 	@Input() countries: Array<Country>;
-
+	countriesBeauty = "";
+	displayedCountryCount = 0;
 	constructor() {}
 
 	ngOnInit() {}
+
+	setdisplayedCountiresBeauty() {
+		let countryList = [];
+		let totalChar = 0;
+		this.countries.forEach((country) => {
+			if (totalChar + country.name.length < 30) {
+				countryList.push(country.name);
+				totalChar += country.name.length;
+			} else {
+				return;
+			}
+		});
+		this.countriesBeauty = countryList.join(", ");
+		if (countryList.length < this.countries.length) {
+			this.countriesBeauty += "...";
+		}
+		this.displayedCountryCount = countryList.length;
+	}
+	ngOnChanges() {
+		this.setdisplayedCountiresBeauty();
+	}
 }
